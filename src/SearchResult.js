cf6ee01828ea1e21820fe8cd918cdfeb9e9448c8
John Petersson <johnpetersson@gmail.com>
2018-05-23 15:38:40 +0200

    Sarch result spacing.

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -4,14 +4,29 @@ export default class SearchResult extends React.Component {
 
   render() {
     return (
-      <div>
-        <h4>
+      <div style={styles.container}>
+        <div style={styles.title}>
           {this.props.song ? this.props.song.title : null}
-        </h4>
-        <h6>
+        </div>
+        <div style={styles.artist}>
           {this.props.song ? this.props.song.artist : null}
-        </h6>
+        </div>
       </div>
     );
+  }
+}
+
+const styles = {
+  container: {
+    marginTop: 'calc(4px + 0.25vmax)',
+    marginBottom: 'calc(6px + 0.25vmax)'
+  },
+  title: {
+    fontSize: 'calc(16px + 0.25vw)',
+    fontWeight: 'bold',
+    marginBottom: 'calc(4px + 0.25vmax'
+  },
+  artist: {
+    fontSize: 'calc(12px + 0.25vw)'
   }
 }
